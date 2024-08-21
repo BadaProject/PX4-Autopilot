d86687c38d05272a0c64833ae64140c308d6a0f8
jeyong <jeyong@gmail.com>
2024-08-21 15:07:06 +0900

    add struct for plc

--- src/modules/plclink/plclink.h
+++ src/modules/plclink/plclink.h
@@ -45,6 +45,150 @@
 #include <uORB/Subscription.hpp>
 #include <uORB/SubscriptionInterval.hpp>
 
+
+typedef struct _PlcToVcc
+{
+    uint16_t mr_mtr_sta; // right main motor status
+    uint16_t mr_flt_msg_err1; // right main motor fault message error 1
+    uint16_t mr_flt_msg_err2; // right main motor fault message error 2
+    uint16_t mr_flt_msg_warn1; // right main motor fault message warning 1
+    uint16_t mr_flt_msg_warn2; // right main motor fault message warning 2
+    uint16_t mr_mtr_curr_real; // right main motor current real
+    uint16_t mr_temp; // right main motor temperature
+    uint16_t mr_mtr_rpm_real; // right main motor RPM real
+    uint16_t mr_mtr_rot_real; // right main motor rotation real
+    uint16_t rsv3509;
+
+    uint16_t ml_mtr_sta; // left main motor status
+    uint16_t ml_flt_msg_err1; // left main motor fault message error 1
+    uint16_t ml_flt_msg_err2; // left main motor fault message error 2
+    uint16_t ml_flt_msg_warn1; // left main motor fault message warning 1
+    uint16_t ml_flt_msg_warn2; // left main motor fault message warning 2
+    uint16_t ml_mtr_curr_real; // left main motor current real
+    uint16_t ml_temp; // left main motor temperature
+    uint16_t ml_mtr_rpm_real; // left main motor RPM real
+    uint16_t ml_mtr_rot_real; // left main motor rotation real
+    uint16_t rsv3519;
+
+    uint16_t br_mtr_sta; // right bow motor status
+    uint16_t br_flt_msg; // right bow motor fault message
+    uint16_t br_mtr_curr_real; // right bow motor current real
+    uint16_t br_temp; // right bow motor temperature
+    uint16_t br_mtr_rpm; // right bow motor RPM
+    uint16_t br_mtr_rot_sta; // right bow motor rotation status
+    uint16_t rsv3526;
+    uint16_t rsv3527;
+    uint16_t rsv3528;
+    uint16_t rsv3529;
+
+    uint16_t bl_mtr_sta; // left bow motor status
+    uint16_t bl_flt_msg; // left bow motor fault message
+    uint16_t bl_mtr_curr_real; // left bow motor current real
+    uint16_t bl_temp; // left bow motor temperature
+    uint16_t bl_mtr_rpm; // left bow motor RPM
+    uint16_t bl_mtr_rot_sta; // left bow motor rotation status
+    uint16_t rsv3536;
+    uint16_t rsv3537;
+    uint16_t rsv3538;
+    uint16_t rsv3539;
+
+    uint16_t sr_str_rpm; // right steering servo RPM
+    int16_t sr_str_ang; // right steering servo angle
+    uint16_t sl_str_rpm; // left steering servo RPM
+    int16_t sl_str_ang; // left steering servo angle
+    uint16_t batt400vdc; // main motor battery 400V
+    uint16_t batt24vdc_1; // ANC PC & PLC
+    uint16_t batt24vdc_2; // bilge pump, always-on power
+} PlcToVcc;
+
+
+typedef struct _VccToPlc
+{
+    uint16_t mr_mtr_run; // right main motor running
+    uint16_t mr_mtr_rpm; // right main motor RPM
+    uint16_t mr_mtr_trq; // right main motor torque
+    uint16_t rsv3003;
+    uint16_t rsv3004;
+    uint16_t rsv3005;
+    uint16_t rsv3006;
+    uint16_t rsv3007;
+    uint16_t rsv3008;
+    uint16_t rsv3009;
+
+    uint16_t ml_mtr_run; // left main motor running
+    uint16_t ml_mtr_rpm; // left main motor RPM
+    uint16_t ml_mtr_trq; // left main motor torque
+    uint16_t rsv3013;
+    uint16_t rsv3014;
+    uint16_t rsv3015;
+    uint16_t rsv3016;
+    uint16_t rsv3017;
+    uint16_t rsv3018;
+    uint16_t rsv3019;
+
+    uint16_t br_mtr_rot; // right bow motor rotation
+    uint16_t br_mtr_thr_per; // right bow motor thrust percentage
+    uint16_t spare3022;
+    uint16_t spare3023;
+    uint16_t spare3024;
+    uint16_t spare3025;
+    uint16_t spare3026;
+    uint16_t spare3027;
+    uint16_t spare3028;
+    uint16_t spare3029;
+
+    uint16_t bl_mtr_rot; // left bow motor rotation
+    uint16_t bl_mtr_thr_per; // left bow motor thrust percentage
+    uint16_t spare3032;
+    uint16_t spare3033;
+    uint16_t spare3034;
+    uint16_t spare3035;
+    uint16_t spare3036;
+    uint16_t spare3037;
+    uint16_t spare3038;
+    uint16_t spare3039;
+
+    uint16_t sr_str_srv_on; // right steering servo on/off
+    uint16_t st_str_srv_home; // right steering servo home
+    uint16_t sr_str_rpm; // right steering servo RPM
+    uint16_t sr_str_ang; // right steering servo angle
+    uint16_t sr_rfrg; // right steering servo refrigerator
+    uint16_t spare3045;
+    uint16_t spare3046;
+    uint16_t spare3047;
+    uint16_t spare3048;
+    uint16_t spare3049;
+
+    uint16_t sl_str_srv_on; // left steering servo on/off
+    uint16_t sl_str_srv_home; // left steering servo home
+    uint16_t sl_str_rpm; // left steering servo RPM
+    uint16_t sl_str_ang; // left steering servo angle
+    uint16_t sl_rfrg; // left steering servo refrigerator
+    uint16_t spare3055;
+    uint16_t spare3056;
+    uint16_t spare3057;
+    uint16_t spare3058;
+    uint16_t spare3059;
+
+    uint16_t emer_stop; // emergency stop
+    uint16_t nav_mode; // navigation mode
+    uint16_t nav_light1; // navigation light 1
+    uint16_t nav_light2; // navigation light 2
+    uint16_t br_s1; // right bilge pump s1
+    uint16_t br_s2; // right bilge pump s2
+    uint16_t br_s3; // right bilge pump s3
+    uint16_t br_s4; // right bilge pump s4
+    uint16_t pr_s1; // right fan s1
+    uint16_t pr_s2; // right fan s2
+
+    uint16_t bl_s1; // left bilge pump s1
+    uint16_t bl_s2; // left bilge pump s2
+    uint16_t bl_s3; // left bilge pump s3
+    uint16_t bl_s4; // left bilge pump s4
+    uint16_t pl_s1; // left fan s1
+    uint16_t pl_s2; // left fan s2
+} VccToPlc;
+
 extern "C" __EXPORT int plclink_main(int argc, char *argv[]);
 
 using namespace time_literals;
